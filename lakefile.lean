ec682c6ad1b083a324e71bab1f5b76466018ff08
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-08-25 07:12:20 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -3,6 +3,9 @@ open Lake DSL
 
 package «SingmastersConjecture» where
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.33.0"
+
 @[default_target]
 lean_lib «SingmastersConjecture» where
   srcDir := "."
